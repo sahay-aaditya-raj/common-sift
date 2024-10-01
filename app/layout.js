f9aeb58b2070f0a358597109b6254afdb42c2638
sahay-aaditya-raj <sahay.aaditya.raj@gmail.com>
2024-10-02 01:54:06 +0530

    google search

--- app/layout.js
+++ app/layout.js
@@ -5,39 +5,37 @@ const inter = Inter({ subsets: ["latin"] });
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
-// export const metadata = {
-//   title: "Home | BrickBlocks",
-//   description: "Ecomerce admin panel",
-//   content:"width=device-width, initial-scale=1"
-// };
 
-
-export default function RootLayout({ children }) {
-
-  return (
-    <html lang="en">
-
-      <body className={`bg-zinc-100 ${inter.className}`}>
-          <div className="flex flex-col min-h-screen max-h-screen">
-            {/* Top Navbar */}
-            <Header />
-            {/* Main Content */}
-            <main>
-              {children}
-            </main>
-            {/* Footer */}
-            <Footer />
-          </div>
-          <ScrollToTopButton/>
-      </body>
-    </html>
-  );
-}
-
-
+// Add your metadata
 export async function generateMetadata() {
   return {
     title: 'Home | Common Sift',
     description: 'Explore our product catalog.',
+    verification: {
+      google: "SfqKenUzEAaGPY7luTx_A0vmXo3-M4IATYMu4NF_mB0",
+    },
+    keywords: "ecommerce, quick, commerce, home, services, online, shopping, cleaning, repair, maintenance",
+    robots: "index, follow",
   };
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="SfqKenUzEAaGPY7luTx_A0vmXo3-M4IATYMu4NF_mB0" />
+      </head>
+      <body className={`bg-zinc-100 ${inter.className}`}>
+        <div className="flex flex-col min-h-screen max-h-screen">
+          {/* Top Navbar */}
+          <Header />
+          {/* Main Content */}
+          <main>{children}</main>
+          {/* Footer */}
+          <Footer />
+        </div>
+        <ScrollToTopButton />
+      </body>
+    </html>
+  );
 }
---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://common-sift.vercel.app/', // Replace with your actual domain
+    generateRobotsTxt: true, // (optional) Generate robots.txt file
+    // Other options can be configured as needed
+  };
+  
